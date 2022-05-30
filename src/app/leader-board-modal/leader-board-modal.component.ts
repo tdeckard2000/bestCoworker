1d263da0f5e31ee30b1d5361d5ec5014f87673cf
Trent <tdeckard2000@yahoo.com>
2022-05-30 12:07:05 -0600

    Get leaderboard working

--- src/app/leader-board-modal/leader-board-modal.component.ts
+++ src/app/leader-board-modal/leader-board-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatestWith } from 'rxjs';
 import { Persons, VoteOptions } from 'server/dbCollectionsInterface';
 import { MainService } from '../services/main.service';
 import { ModalServiceService } from '../services/modal-service.service';
@@ -13,21 +14,22 @@ export class LeaderBoardModalComponent implements OnInit {
   constructor(private modalService: ModalServiceService, private mainService: MainService) { }
 
   allVoteOptions: Array<VoteOptions> | null = null;
+  highestVotes: {[key:string]: {name: string, number: number}} = {};
 
   determineLeaders(allPersons: Array<Persons>, voteOptions: Array<VoteOptions>) {
-    let highestVotes: {[key:string]: number} = {};
-
     for(let person of allPersons) {
       for(let voteOption of voteOptions) {
         let voteOptionName = voteOption.name.toLowerCase().replace(/\s+/g, '');
-        if(!highestVotes[voteOptionName]) {
-          highestVotes[voteOptionName] = 0;
-        } else {
-          console.log(highestVotes)
+        if(!this.highestVotes[voteOptionName]) {
+          this.highestVotes[voteOptionName] = {name: '', number: 0};
+        }
+        if (person.votes[voteOptionName] && (person.votes[voteOptionName] > this.highestVotes[voteOptionName].number)) {
+          this.highestVotes[voteOptionName].name = person.name;
+          this.highestVotes[voteOptionName].number = person.votes[voteOptionName];
         }
       }
     }
-    console.log(highestVotes)
+    console.log(this.highestVotes)
   };
 
   onCloseModal() {
@@ -35,14 +37,13 @@ export class LeaderBoardModalComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    this.mainService.getAllPersons().subscribe((allPersons) => {
-      console.log("GOT PERSONS")
-      this.mainService.getAllVoteOptions().subscribe((voteOptions) => {
-        console.log("GOT VOTE OPTIONS")
-        this.allVoteOptions = voteOptions;
-        this.determineLeaders(allPersons, voteOptions);
-      });
-    })
+    const $allPersons = this.mainService.getAllPersons();
+    const $voteOptions = this.mainService.getAllVoteOptions();
+    $allPersons.pipe(combineLatestWith($voteOptions)).subscribe(([allPersons, voteOptions]) => {
+      this.allVoteOptions = voteOptions;
+      this.determineLeaders(allPersons, voteOptions);
+    });
+
   }
 
 }
